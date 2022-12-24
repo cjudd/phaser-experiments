2b3277d855a06cd804bcc4afa9985dc17b258f9b
Christopher M. Judd <javajudd@gmail.com>
2022-12-23 20:18:58 -0500

    Basic starting point for a Phaser.io project/experiment.

---
+++ scripts/GameScene.js
@@ -0,0 +1,18 @@
+class GameScene extends Phaser.Scene {
+    constructor() {
+        super('GameScene')
+    }
+
+    preload() {
+
+    } //end preload
+
+    create() {
+
+    } //end create
+
+    update() {
+
+    } //end update
+
+} //end gameScene
